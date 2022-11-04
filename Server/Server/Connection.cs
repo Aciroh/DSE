84bc8bad07d6b3544413f96dd120d02c977c1d8f
Aciroh <93126326+Aciroh@users.noreply.github.com>
2022-11-05 00:50:17 +0200

    Add methods in Connection class
    Nothing to describe. Just the method bodies, no implementation.

--- Server/Server/Connection.cs
+++ Server/Server/Connection.cs
@@ -8,9 +8,32 @@ namespace Server
 {
     internal class Connection
     {
-        public void Listen()
+        Connection(int listenPort, int streamPort)
+        {
+            this.listenPort = listenPort;
+            this.streamPort = streamPort;
+        }
+
+        private int listenPort;
+        private int streamPort;
+
+
+        public void StartServer()
         {
 
+        }
+
+        internal void CheckForOtherServers()
+        {
+            //This must be ran first, to check if there is another server on the network. If there is, cancel the execution and return exception or whatever
+        }
+
+        public void ListenForBroadcast()
+        {
+            while(true)
+            {
+
+            }
         }
 
 
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,15 +1,16 @@
-﻿
-using Server;
+﻿using Server;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Connection con = new Connection();
 
         Config config = new();
         config.ReadConfig();
         config.PrintConfig();
+
+        //Connection con = new Connection();
+
     }
 }
 
