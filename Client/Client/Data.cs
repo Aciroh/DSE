d0af84fb335bd85b8a08f24d294060990c325c66
Tiboldi_Roberto <83965956+TiboldiRoberto@users.noreply.github.com>
2022-11-10 10:07:54 +0200

    Add configuration, output file and code for starting Simulator execution

---
+++ Client/Client/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Data
+    {
+        //Execution
+        public int superscalarFactor { get; set; } = 1; 
+        public int numberOfRenameEntities { get; set; } = 1;
+        public int ReorderEntries { get; set; } = 1;
+
+        //General
+        public int NumberOfReservationStationsPerBuffer { get; set; } = 1;
+        public int NumberOfIntegerExecutionUnits { get; set; } = 1;
+        public int NumberOfFloatingPointExecutionUnits { get; set; } = 1;
+        public int NumberOfBranchExecutionUnits { get; set; } = 1;
+		public int NumberOfMemoryExecutionUnits { get; set; } = 1;
+
+
+
+    }
+}
--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Xml.Serialization;
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        String config = "C:\\Users\\Roberto\\Desktop\\Proiect Florea\\DSE\\Client\\Client\\default_cfg.xml";
+        String output = "C:\\Users\\Roberto\\Desktop\\Proiect Florea\\DSE\\Client\\Client\\output.xml";
+
+        Directory.SetCurrentDirectory(@"C:\Program Files (x86)\PSATSim");
+
+        ProcessStartInfo processStartInfo = new ProcessStartInfo();
+
+        processStartInfo.FileName = "psatsim_con.exe";
+        processStartInfo.ArgumentList.Add(config);
+        processStartInfo.ArgumentList.Add(output);
+        Process.Start(processStartInfo);
+    }
+}
