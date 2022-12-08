877bbd6b9bec26ddfa5ea2c25f607455f7a09c1d
Aciroh <horatiubolovan@gmail.com>
2022-12-08 15:35:00 +0200

    Actually last commit

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -13,53 +13,54 @@ internal class Program
         Config config = new Config();
         Connection connection = new Connection(config.GetPort(), config.GetPort());
 
-        String outputPath = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).FullName).FullName + @"/output.xml";
-        String configPath = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).FullName).FullName + @"/default_cfg.xml";
 
-        FileManager configFileManager = new FileManager(configPath);
-        ConfigurationData configurationData = new ConfigurationData();
+        //String outputPath = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).FullName).FullName + @"/output.xml";
+        //String configPath = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).FullName).FullName + @"/default_cfg.xml";
 
-        configFileManager.UpdateAttribute(0, configurationData.configurationName, configurationData.config);
+        //FileManager configFileManager = new FileManager(configPath);
+        //ConfigurationData configurationData = new ConfigurationData();
 
-        configFileManager.UpdateAttribute(0, configurationData.superscalar, configurationData.general);
-        configFileManager.UpdateAttribute(1, configurationData.rename, configurationData.general);
-        configFileManager.UpdateAttribute(2, configurationData.reorder, configurationData.general);
-        configFileManager.UpdateAttribute(3, configurationData.rsb_architecture, configurationData.general);
-        configFileManager.UpdateAttribute(4, configurationData.rs_per_rsb, configurationData.general);
-        configFileManager.UpdateAttribute(5, configurationData.speculative, configurationData.general);
-        configFileManager.UpdateAttribute(6, configurationData.speculation_accuracy, configurationData.general);
-        configFileManager.UpdateAttribute(7, configurationData.separate_dispatch, configurationData.general);
-        configFileManager.UpdateAttribute(8, configurationData.seed, configurationData.general);
-        configFileManager.UpdateAttribute(9, configurationData.trace, configurationData.general);
-        configFileManager.UpdateAttribute(10, configurationData.output, configurationData.general);
-        configFileManager.UpdateAttribute(11, configurationData.vdd, configurationData.general);
-        configFileManager.UpdateAttribute(12, configurationData.frequency, configurationData.general);
+        //configFileManager.UpdateAttribute(0, configurationData.configurationName, configurationData.config);
 
-        configFileManager.UpdateAttribute(0, configurationData.architecturee, configurationData.execution);
-        configFileManager.UpdateAttribute(1, configurationData.integer, configurationData.execution);
-        configFileManager.UpdateAttribute(2, configurationData.floating, configurationData.execution);
-        configFileManager.UpdateAttribute(3, configurationData.branch, configurationData.execution);
-        configFileManager.UpdateAttribute(4, configurationData.memoryp, configurationData.execution);
+        //configFileManager.UpdateAttribute(0, configurationData.superscalar, configurationData.general);
+        //configFileManager.UpdateAttribute(1, configurationData.rename, configurationData.general);
+        //configFileManager.UpdateAttribute(2, configurationData.reorder, configurationData.general);
+        //configFileManager.UpdateAttribute(3, configurationData.rsb_architecture, configurationData.general);
+        //configFileManager.UpdateAttribute(4, configurationData.rs_per_rsb, configurationData.general);
+        //configFileManager.UpdateAttribute(5, configurationData.speculative, configurationData.general);
+        //configFileManager.UpdateAttribute(6, configurationData.speculation_accuracy, configurationData.general);
+        //configFileManager.UpdateAttribute(7, configurationData.separate_dispatch, configurationData.general);
+        //configFileManager.UpdateAttribute(8, configurationData.seed, configurationData.general);
+        //configFileManager.UpdateAttribute(9, configurationData.trace, configurationData.general);
+        //configFileManager.UpdateAttribute(10, configurationData.output, configurationData.general);
+        //configFileManager.UpdateAttribute(11, configurationData.vdd, configurationData.general);
+        //configFileManager.UpdateAttribute(12, configurationData.frequency, configurationData.general);
 
-        configFileManager.UpdateAttribute(0, configurationData.architecture, configurationData.memory);
+        //configFileManager.UpdateAttribute(0, configurationData.architecturee, configurationData.execution);
+        //configFileManager.UpdateAttribute(1, configurationData.integer, configurationData.execution);
+        //configFileManager.UpdateAttribute(2, configurationData.floating, configurationData.execution);
+        //configFileManager.UpdateAttribute(3, configurationData.branch, configurationData.execution);
+        //configFileManager.UpdateAttribute(4, configurationData.memoryp, configurationData.execution);
 
-        configFileManager.UpdateAttribute(0, configurationData.architecture, configurationData.memory);
-        configFileManager.UpdateAttribute(0, configurationData.hitratel1c, configurationData.memoryL1code);
-        configFileManager.UpdateAttribute(1, configurationData.latencyl1c, configurationData.memoryL1code);
-        configFileManager.UpdateAttribute(0, configurationData.hitratel1d, configurationData.memoryL1data);
-        configFileManager.UpdateAttribute(1, configurationData.latencyl1d, configurationData.memoryL1data);
-        configFileManager.UpdateAttribute(0, configurationData.hitratel2, configurationData.memoryL2);
-        configFileManager.UpdateAttribute(1, configurationData.latencyl2, configurationData.memoryL2);
-        configFileManager.UpdateAttribute(0, configurationData.latency, configurationData.system);
+        //configFileManager.UpdateAttribute(0, configurationData.architecture, configurationData.memory);
 
-        //Directory.SetCurrentDirectory(@"C:\Program Files (x86)\PSATSim");
-        Directory.SetCurrentDirectory(Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).FullName).FullName + @"/Tools/PSATSim");
+        //configFileManager.UpdateAttribute(0, configurationData.architecture, configurationData.memory);
+        //configFileManager.UpdateAttribute(0, configurationData.hitratel1c, configurationData.memoryL1code);
+        //configFileManager.UpdateAttribute(1, configurationData.latencyl1c, configurationData.memoryL1code);
+        //configFileManager.UpdateAttribute(0, configurationData.hitratel1d, configurationData.memoryL1data);
+        //configFileManager.UpdateAttribute(1, configurationData.latencyl1d, configurationData.memoryL1data);
+        //configFileManager.UpdateAttribute(0, configurationData.hitratel2, configurationData.memoryL2);
+        //configFileManager.UpdateAttribute(1, configurationData.latencyl2, configurationData.memoryL2);
+        //configFileManager.UpdateAttribute(0, configurationData.latency, configurationData.system);
+
+        ////Directory.SetCurrentDirectory(@"C:\Program Files (x86)\PSATSim");
+        //Directory.SetCurrentDirectory(Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).FullName).FullName + @"/Tools/PSATSim");
 
 
-        ProcessStartInfo processStartInfo = new ProcessStartInfo();
-        processStartInfo.FileName = "psatsim_con.exe";
-        processStartInfo.ArgumentList.Add(configPath);
-        processStartInfo.ArgumentList.Add(outputPath);
-        Process.Start(processStartInfo);
+        //ProcessStartInfo processStartInfo = new ProcessStartInfo();
+        //processStartInfo.FileName = "psatsim_con.exe";
+        //processStartInfo.ArgumentList.Add(configPath);
+        //processStartInfo.ArgumentList.Add(outputPath);
+        //Process.Start(processStartInfo);
     }
 }
