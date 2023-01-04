70d6f942237161a19a1b0b56327b9fabeca19179
Horatiu Bolovan <93126326+Aciroh@users.noreply.github.com>
2023-01-04 19:46:05 +0200

    Fix one exception

--- Server/Server/Connection.cs
+++ Server/Server/Connection.cs
@@ -127,21 +127,29 @@ namespace Server
                 int i;
 
                 // Loop to receive all the data sent by the client.
-                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                try
                 {
-                    // Translate data bytes to a ASCII string.
-                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                    Console.WriteLine("Received: {0}", data);
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
 
-                    // Process the data sent by the client.
-                    data = data.ToUpper();
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
 
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
 
-                    // Send back a response.
-                    stream.Write(msg, 0, msg.Length);
-                    Console.WriteLine("Sent: {0}", data);
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
+                    }
                 }
+                catch
+                {
+                    tcpClients.Remove(client);
+                }
+                
             }
         }
 
